c6c82eeb86d1e16604f2f973bbfe285ca48fddc8
Shahajadahasib <shahajadahasib@gmail.com>
2022-11-17 23:26:48 +0600

    error

--- lib/audiolist.dart
+++ lib/audiolist.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:music/provider.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:provider/provider.dart';
+
 import 'audio.dart';
-import 'permissio.dart';
 
 class AudioList extends StatelessWidget {
   const AudioList({Key? key}) : super(key: key);
@@ -15,63 +14,70 @@ class AudioList extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     context.read<AudioProvider>().fetchAudioList;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Music Player"),
-          elevation: 2,
-        ),
-        body: context.read<AudioProvider>().audioList.isNotEmpty // must read
-            ? Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: context.read<AudioProvider>().audioList.length,
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            // context.read<AudioProvider>().currentSongIndex =
-                            //     index;
-                            // showCupertinoModalBottomSheet(
-                            //   context: context,
-                            //   builder: (context) => const Audio(),
-                            // );
-                          },
-                          child: ListTile(
-                            title: Text(context
-                                .read<AudioProvider>()
-                                .audioList[index]
-                                .title),
-                            subtitle: Text(context
-                                    .read<AudioProvider>()
-                                    .audioList[index]
-                                    .artist ??
-                                "No Artist"),
-                            trailing: const Icon(Icons.arrow_forward_rounded),
-                            // This Widget will query/load image. Just add the id and type.
-                            // You can use/create your own widget/method using [queryArtwork].
-                            leading: QueryArtworkWidget(
-                              id: context
+      appBar: AppBar(
+        title: const Text("Music Player"),
+        elevation: 2,
+      ),
+      body: context.read<AudioProvider>().audioList.isNotEmpty // must read
+          ? Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: context.read<AudioProvider>().audioList.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          context.read<AudioProvider>().currentSongIndex =
+                              index;
+                          showCupertinoModalBottomSheet(
+                            context: context,
+                            builder: (context) => const Audio(),
+                          );
+                        },
+                        child: ListTile(
+                          title: Text(context
+                              .read<AudioProvider>()
+                              .audioList[index]
+                              .title),
+                          subtitle: Text(context
                                   .read<AudioProvider>()
                                   .audioList[index]
-                                  .id,
-                              type: ArtworkType.AUDIO,
-                            ),
+                                  .artist ??
+                              "No Artist"),
+                          trailing: const Icon(Icons.arrow_forward_rounded),
+                          // This Widget will query/load image. Just add the id and type.
+                          // You can use/create your own widget/method using [queryArtwork].
+                          leading: QueryArtworkWidget(
+                            id: context
+                                .read<AudioProvider>()
+                                .audioList[index]
+                                .id,
+                            type: ArtworkType.AUDIO,
                           ),
-                        );
-                      },
-                    ),
+                        ),
+                      );
+                    },
                   ),
-                  // !context.watch<AudioProvider>().isPlaying
-                  //     ? _buildPlayingCard(
-                  //         context: context,
-                  //         size: size,
-                  //       )
-                  //     : const SizedBox(),
-                ],
-              )
-            : const Center(
-                child: CircularProgressIndicator(
-                  semanticsLabel: 'Loading',
                 ),
-              ));
+                // context.read<AudioProvider>().isPlaying
+                //     ? Container(
+                //         color: Colors.red,
+                //         height: 50,
+                //         child: Text(context
+                //             .read<AudioProvider>()
+                //             .audioList[context
+                //                 .watch<AudioProvider>()
+                //                 .currentSongIndex]
+                //             .title),
+                //       )
+                //     : SizedBox()   : const SizedBox(),
+              ],
+            )
+          : const Center(
+              child: CircularProgressIndicator(
+                semanticsLabel: 'Loading',
+              ),
+            ),
+    );
   }
 }
